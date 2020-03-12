0f7b76a2a47e5a1cf57b6c8fb32374406782efde
Ruari Day <ruariday@gmail.com>
2020-03-12 13:14:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for]
+programmer_hash[:yukihiro_matsumoto] =
+  :known_for 
     
 end
 
