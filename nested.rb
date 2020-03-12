faf3eccfa45e0ad7064ccf7a89ac89a668cd00c7
Ruari Day <ruariday@gmail.com>
2020-03-12 13:17:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -122,5 +123,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay][:known_for] = "GUI"
-programmer_hash
+end 
